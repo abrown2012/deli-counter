ab376bde90a0fc5a1248d9aec36efe6ce74ab942
Agnieszka Brown <abrown2012@icloud.com>
2020-10-22 22:22:56 -0400

    commit tree 611b0d4c5333357cc70420c5f88bd62d1a8fb6f1

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+     
+    if katz_deli.count == 0 
+        puts "The line is currently empty."
+    else 
+        i = 1
+        line = "The line is currently:"
+        while i <= katz_deli.count do 
+        name = katz_deli[i-1]
+        line = "#{line} #{i}. #{name}"
+        i += 1
+        end 
+    puts line
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+    x = katz_deli.count
+    puts "Welcome, #{name}. You are number #{x} in line."
+end
+
+def now_serving(katz_deli)
+    x = katz_deli[0]
+    katz_deli.shift()
+    if katz_deli.count == 0 
+        puts 'There is nobody waiting to be served!'
+    else puts "Currently serving #{x}."
+    end
+end
